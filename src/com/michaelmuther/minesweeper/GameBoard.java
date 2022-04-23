fd0bc293e0aa85acdabca2a11589b0671e893a6a
Michael Muther <michaelmuther@yahoo.com>
2022-04-23 20:06:04 +0200

    Stage 3 completed

--- src/com/michaelmuther/minesweeper/GameBoard.java
+++ src/com/michaelmuther/minesweeper/GameBoard.java
@@ -6,6 +6,8 @@ public class GameBoard {
     private final int BOARD_HEIGHT = 9;
     private final int BOARD_WIDTH = 9;
     private final char[][] gameBoard = new char[BOARD_HEIGHT][BOARD_WIDTH];
+    private final char MINE = 'X';
+    private final char SPACE = '.';
 
     public GameBoard(int mines) {
         this.mines = mines;
@@ -13,8 +15,6 @@ public class GameBoard {
     }
 
     private void generateGameBoard() {
-        final char MINE = 'X';
-        final char SPACE = '.';
         double mineFactor = (double) mines / (double) (BOARD_HEIGHT * BOARD_WIDTH);
 //        System.out.println(mineFactor); // TEST
         int totalMines;
@@ -33,6 +33,72 @@ public class GameBoard {
             }
 //            System.out.println(totalMines); // TEST
         } while (mines != totalMines);
+        updateMineCounter();
+    }
+
+    private void updateMineCounter() {
+        for (int i = 0; i < BOARD_HEIGHT; i++) {
+            for (int j = 0; j < BOARD_WIDTH; j++) {
+                if (gameBoard[i][j] == SPACE) {
+                    countMines(i, j);
+                }
+            }
+        }
+    }
+
+    private void countMines(int index1, int index2) {
+        int neighborMines = 0;
+        // check upper left
+        if (index1 >= 1 && index2 >= 1) {
+            if (gameBoard[index1 - 1][index2 - 1] == MINE) {
+                neighborMines++;
+            }
+        }
+        // check above
+        if (index2 >= 1) {
+            if (gameBoard[index1][index2 - 1] == MINE) {
+                neighborMines++;
+            }
+        }
+        // check upper right
+        if (index1 <= 7 && index2 >= 1) {
+            if (gameBoard[index1 + 1][index2 - 1] == MINE) {
+                neighborMines++;
+            }
+        }
+        // check right
+        if (index1 <= 7) {
+            if (gameBoard[index1 + 1][index2] == MINE) {
+                neighborMines++;
+            }
+        }
+        // check below right
+        if (index1 <= 7 && index2 <= 7) {
+            if (gameBoard[index1 + 1][index2 + 1] == MINE) {
+                neighborMines++;
+            }
+        }
+        // check below
+        if (index2 <= 7) {
+            if (gameBoard[index1][index2 + 1] == MINE) {
+                neighborMines++;
+            }
+        }
+        // check below left
+        if (index1 >= 1 && index2 <= 7) {
+            if (gameBoard[index1 - 1][index2 + 1] == MINE) {
+                neighborMines++;
+            }
+        }
+        // check left
+        if (index1 >= 1) {
+            if (gameBoard[index1 - 1][index2] == MINE) {
+                neighborMines++;
+            }
+        }
+        if (neighborMines > 0) {
+            gameBoard[index1][index2] = (char)(neighborMines + '0');
+        }
     }
 
     public char[][] getGameBoard() {
